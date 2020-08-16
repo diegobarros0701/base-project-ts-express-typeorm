172ee8ff5641446a122ae69110dc5a161e2163b0
Diego Barros <diegobarros2812@gmail.com>
2020-08-16 13:23:37 -0400

    Updating prettier and ormconfig

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,8 +1,12 @@
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
 module.exports = {
   type: 'postgres',
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  entities: ['src/database/**/*.model.ts'],
+  namingStrategy: new SnakeNamingStrategy()
 };
